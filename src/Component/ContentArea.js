1d49bd627fc16847ab1a6de485838214ccf5c271
Ziye Wang <zwang@LT-C02SF4R4G8WP.local>
2019-02-01 14:16:55 -0500

    apply HOC for consumers

--- src/Component/ContentArea.js
+++ src/Component/ContentArea.js
@@ -1,28 +1,28 @@
-import React, { useContext } from 'react';
-import Context from '../Context/Context';
-import './ContentArea.css';
+import React from "react";
+import { compose } from "recompose";
+import withContext from "../Hoc/withContext";
+import "./ContentArea.css";
 
-const ContentArea = () => {
-  const { theme, themeDispacher } = useContext(Context);
-  return (
-    <>
-      <div onClick={() => themeDispacher({ type: 'TOGGLE_THEME' })}>
-        switch theme
-      </div>
-      <div className={`container ${theme}`}>
-        <div className="box">box1</div>
-        <div className="box">box2</div>
-        <div className="box">box3</div>
-        <div className="box">box4</div>
-        <div className="box">box5</div>
-        <div className="box">box6</div>
-        <div className="box">box7</div>
-        <div className="box">box8</div>
-        <div className="box">box9</div>
-        <div className="box">box10</div>
-      </div>
-    </>
-  );
-};
+const ContentArea = ({ theme, themeDispacher }) => (
+  <>
+    <div onClick={() => themeDispacher({ type: "TOGGLE_THEME" })}>
+      switch theme
+    </div>
+    <div className={`container ${theme}`}>
+      <div className="box">box1</div>
+      <div className="box">box2</div>
+      <div className="box">box3</div>
+      <div className="box">box4</div>
+      <div className="box">box5</div>
+      <div className="box">box6</div>
+      <div className="box">box7</div>
+      <div className="box">box8</div>
+      <div className="box">box9</div>
+      <div className="box">box10</div>
+    </div>
+  </>
+);
 
-export default ContentArea;
+const ConentAreaWithContext = compose(withContext)(ContentArea);
+
+export default ConentAreaWithContext;
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,17 +1,17 @@
-import React, { useContext } from 'react';
-import Context from '../Context/Context';
-import './Header.css';
+import React from "react";
+import { compose } from "recompose";
+import withContext from "../Hoc/withContext";
+import "./Header.css";
 
-const Header = () => {
-  const { language } = useContext(Context);
-  return (
-    <>
-      <div className="header">
-        <div className="language_label">{language}</div>
-        <div className="switch_language">switch language</div>
-      </div>
-    </>
-  );
-};
+const Header = ({ language }) => (
+  <>
+    <div className="header">
+      <div className="language_label">{language}</div>
+      <div className="switch_language">switch language</div>
+    </div>
+  </>
+);
 
-export default Header;
+const HeaderWithContext = compose(withContext)(Header);
+
+export default HeaderWithContext;
