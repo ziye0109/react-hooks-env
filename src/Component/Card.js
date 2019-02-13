297246033e1126d44153e7c85e0b2332f7ab46d3
Ziye Wang <zwang@LT-C02SF4R4G8WP.local>
2019-02-13 16:38:27 -0500

    add  use Previous hook

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,14 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import usePrevious from '../CustomHooks/usePrevious';
 import './Card.css';
 
+
 const Card = () => {
+  const [state, setCount] = useState({ count: 0 });
+
+  const preState = usePrevious(state.count);
+
   useEffect(() => {
     console.log('mounted!');
   }, []);
 
+  useEffect(() => {
+    console.log('updated if count changes!');
+    console.log(`pre: ${preState} cur: ${state.count}`);
+  }, [state.count]);
+
   return (
     <>
-      <div className="card">card</div>
+      <div className="card">
+        <div>{state.count}</div>
+        <div>
+          <div
+            className="count_button"
+            onClick={() => {
+              setCount(value => ({ count: value.count + 1 }));
+            }}
+          >
+            increment count
+          </div>
+          <div
+            className="count_button"
+            onClick={() => setCount(value => ({ count: value.count - 1 }))}
+          >
+            decrement count
+          </div>
+        </div>
+      </div>
     </>
   );
 };
