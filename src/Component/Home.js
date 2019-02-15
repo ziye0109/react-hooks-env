05c85c36fc14f75adff87aeff2b958dd6eeb70aa
Ziye Wang <zwang@LT-C02SF4R4G8WP.local>
2019-02-15 16:23:43 -0500

    router to content area

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import Header from './Header';
-import ContentArea from './ContentArea';
+import React from "react";
+import Header from "./Header";
+import DashBoard from "./DashBoard";
+import ContentArea from "./ContentArea";
+import { Router, Route, Switch } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
+
+const history = createBrowserHistory();
 
 const Home = () => (
   <>
     <Header />
-    <ContentArea />
+    <Router history={history}>
+      <Switch>
+        <Route path="/dashboard" component={DashBoard} />
+        <Route path="/" component={ContentArea} />
+      </Switch>
+    </Router>
   </>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { Router, Route, Switch } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
-import Home from './Component/Home';
-import DashBoard from './Component/DashBoard';
-import Provider from './Component/Provider';
-
-const history = createBrowserHistory();
+import React from "react";
+import ReactDom from "react-dom";
+import Home from "./Component/Home";
+import Provider from "./Component/Provider";
 
 const App = () => (
   <Provider>
-    <Router history={history}>
-      <Switch>
-        <Route path="/home" component={Home} />
-        <Route path="/dashboard" component={DashBoard} />
-      </Switch>
-    </Router>
+    <Home />
   </Provider>
 );
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById("root"));
